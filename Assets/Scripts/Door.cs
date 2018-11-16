2f1db75c93e835bba512712f4dd38ce8bb748d68
Brandon0606400 <43067373+Brandon0606400@users.noreply.github.com>
2018-11-16 15:37:44 +0000

    Added new level and jump
    Added a door that goes to another level and added a jump button

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Extra using statement to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    //Designer Variables
+    public string sceneToLoad;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    
+    //Check if the thing that we collided with
+    //is the player (aka has the player script)
+    Player playerScript = collision.collider.GetComponent<Player>();
+
+        //Only do someting if the thing we ran into
+        //was the player
+        //aka playerScript is not null
+        if (playerScript != null)
+        {
+            //We did hit the player
+            //Load the next level
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+
+    //unity calls this function automatically
+    //When our water touches any other object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check if the thing that we collided with
+        //is the player (aka has the player script)
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        //Only do someting if the thing we ran into
+        //was the player
+        //aka playerScript is not null
+        if (playerScript != null)
+        {
+            //We did hit the player
+            //Kill them
+            playerScript.Kill();
+        }
+    }
+}
