03aecc7637697c06f7b6d0b24971c4b93b0ecb9c
Brandon0606400 <43067373+Brandon0606400@users.noreply.github.com>
2018-11-30 13:52:22 +0000

    Added start screen
    Added start screen and button that leads to the first level

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This allows us to use the scene loading function
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour{
+
+    // This will be called by the button component
+    // when the button is clicked
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
